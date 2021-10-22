67abd3d9057613b2e342e9ec44499fbba9667a85
水逆 <huyi247767221@163.com>
2021-10-22 16:50:08 +0800

    fix: 校正 cli 版本

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,26 +1,26 @@
 /** 生成模板入参定义 */
 interface IGenerateTemplateProps {
-  /** 项目名称 */
-  name: string;
-  /** 版本号 */
-  version: string;
-  /** 项目描述 */
-  description: string;
-  /** 项目作者 */
-  author: string;
-  /** 项目监听端口 */
-  port?: string;
-  /** 项目模板 */
-  template?: string;
+    /** 项目名称 */
+    name: string;
+    /** 版本号 */
+    version: string;
+    /** 项目描述 */
+    description: string;
+    /** 项目作者 */
+    author: string;
+    /** 项目监听端口 */
+    port?: string;
+    /** 项目模板 */
+    template?: string;
 }
 /** 生成规则入参定义 */
 interface IGenerateRuleProps {
-  /** 是否进行格式化控制 */
-  isPrettier?: boolean;
-  /** 是否进行 eslint 语法检测 */
-  isEslint?: boolean;
-  /** 是否进行代码提交规则检测 */
-  isCommitLint?: boolean;
+    /** 是否进行格式化控制 */
+    isPrettier?: boolean;
+    /** 是否进行 eslint 语法检测 */
+    isEslint?: boolean;
+    /** 是否进行代码提交规则检测 */
+    isCommitLint?: boolean;
 }
 /**
  * 获取模板列表
@@ -34,17 +34,7 @@ export declare const getTemplateQuestionList: () => string[];
  * @param toPath
  * @param param
  */
-export declare const generateTemplate: (
-  toPath: string,
-  {
-    name,
-    version,
-    description,
-    author,
-    template,
-    port,
-  }: IGenerateTemplateProps,
-) => void;
+export declare const generateTemplate: (toPath: string, { name, version, description, author, template, port, }: IGenerateTemplateProps) => void;
 /**
  * 生成规则配置，并抛出相关依赖
  *
@@ -52,8 +42,5 @@ export declare const generateTemplate: (
  * @param param
  * @returns
  */
-export declare const generateRule: (
-  toPath: string,
-  { isPrettier, isEslint, isCommitLint }: IGenerateRuleProps,
-) => Promise<string[]>;
+export declare const generateRule: (toPath: string, { isPrettier, isEslint, isCommitLint, }: IGenerateRuleProps) => Promise<string[]>;
 export {};
